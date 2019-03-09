5775e12c13ccd0fea166da103c35659fcfff4e92
Julien Usson <jusson@agaetis.fr>
2019-03-09 18:36:12 +0100

    :sparkles: use state example

---
+++ examples/useStateExample.js
@@ -0,0 +1,11 @@
+function couter() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={e => setCounter(counter + 1)}>Increment</button>
+      <button onClick={e => setCounter(counter - 1)}>Decrement</button>
+    </div>
+  );
+}
